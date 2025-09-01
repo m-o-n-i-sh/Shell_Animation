a5391aad67a5f7385f76460903b4762a9a67db42
m-o-n-i-sh <mmoniishredde@gmail.com>
2025-09-01 10:17:30 +0530

    Add files via upload

--- Rocket.sh
+++ Rocket.sh
@@ -1,30 +1,51 @@
 #!/bin/bash
-delay=0.2
+delay=0.12
 rocket=(
-"   ^   "
-"  / \\  "
-" |   | "
-" |   | "
-" |___| "
+"\e[37m       /^\      \e[0m"
+"\e[37m      /   \     \e[0m"
+"\e[37m     /     \    \e[0m"
+"\e[34m    |       |   \e[0m"
+"\e[34m    |       |   \e[0m"
+"\e[34m    |       |   \e[0m"
+"\e[34m    |       |   \e[0m"
+"\e[34m    |_______|   \e[0m"
+"\e[33m     /     \    \e[0m"
+"\e[33m    /       \   \e[0m"
 )
-exhaust=("  **  " "  ##  " "  ++  " "      ")
-cols=$(tput cols)
-lines=$(tput lines)
-rocket_height=${#rocket[@]}
-rocket_width=${#rocket[0]}
-pad=$(( (cols - rocket_width) / 2 ))
-for ((pos=lines-rocket_height; pos>=0; pos--)); do
-    clear
-    for ((i=0; i<pos; i++)); do
-        echo
-    done
-    for line in "${rocket[@]}"; do
-        printf "%*s%s\n" $pad "" "$line"
-    done
-    frame=$((pos % ${#exhaust[@]}))
-    printf "%*s%s\n" $pad "" "${exhaust[$frame]}"
-    sleep $delay
+cols=$(tput cols); lines=$(tput lines)
+h=${#rocket[@]}; w=${#rocket[0]}
+pad=$(( (cols - w) / 2 ))
+for c in 3 2 1; do clear; printf "%*s%s\n" $((cols/2)) "" "$c..."; sleep 1; done
+clear; printf "%*s%s\n" $((cols/2)) "" "Ignition!!"; afplay "rocket-launch.mp3" & sleep 1
+for ((pos=lines-h-3; pos>=0; pos--)); do
+  clear
+  for ((i=0;i<pos;i++)); do echo; done
+  for line in "${rocket[@]}"; do printf "%*s%b\n" "$pad" "" "$line"; done
+  f=$((RANDOM%4))
+  case $f in
+    0) tput setaf 7; printf "%*s%s\n" "$pad" "" "      ..^..     "; tput setaf 3; printf "%*s%s\n" "$pad" "" "     .*****.    "; tput setaf 1; printf "%*s%s\n" "$pad" "" "     #######    " ;;
+    1) tput setaf 7; printf "%*s%s\n" "$pad" "" "      .^^^.     "; tput setaf 3; printf "%*s%s\n" "$pad" "" "     *******.   "; tput setaf 1; printf "%*s%s\n" "$pad" "" "     #######    " ;;
+    2) tput setaf 7; printf "%*s%s\n" "$pad" "" "      ..^..     "; tput setaf 3; printf "%*s%s\n" "$pad" "" "      *****     "; tput setaf 1; printf "%*s%s\n" "$pad" "" "      #####     " ;;
+    3) tput setaf 7; printf "%*s%s\n" "$pad" "" "      .^.^.     "; tput setaf 3; printf "%*s%s\n" "$pad" "" "     .*****.    "; tput setaf 1; printf "%*s%s\n" "$pad" "" "    #########   " ;;
+  esac
+  sleep "$delay"
+done
+killall afplay 2>/dev/null; afplay "explosion.mp3" &
+chars=("*" "#" "@" "%" "+" "x")
+for k in {1..04}; do
+  clear
+  for ((r=0;r<lines;r++)); do
+    tput cup $r 0
+    tput setaf $((1+RANDOM%6))
+    line=""
+    for ((c=0;c<cols;c++)); do line+="${chars[$((RANDOM%${#chars[@]}))]}"; done
+    printf "%s" "$line"
+  done
+  tput sgr0
+  sleep 0.08
 done
 clear
-echo "Rocket Launched!"
+tput setaf 2
+figlet "SUSHANT BIO GOD"
+tput sgr0
 
